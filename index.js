fda175d71f07f6559e67a6d22c0fedec11c7f07b
Shreyansh Jain <shhreyanshjain.2015@vit.ac.in>
2018-10-21 12:59:38 +0530

    Add custom user and password

--- index.js
+++ index.js
@@ -13,9 +13,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/api/send', function(req, res) {
     var mobile = req.body.mobile;
     var mess = req.body.mess;
+    var user = req.body.user;
+    var pass = req.body.pass;
 
     const start = async function() {
-    cookie = await way2sms.login('7073112280', '31121992');
+    cookie = await way2sms.login(user.toString(), pass.toString);
     await way2sms.send(cookie, mobile.toString(), mess.toString());
     }
     start()
